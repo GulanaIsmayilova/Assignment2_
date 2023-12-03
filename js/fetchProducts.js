181dc3a147651b2c5a14d846488d9fdb5008f96a
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2023-12-03 22:58:07 +0400

    m

--- js/fetchProducts.js
+++ js/fetchProducts.js
@@ -1,7 +1,11 @@
-
 let url = 'https://dummyjson.com/products';
 
 fetch(url)
-  .then(res => res.json())
+.then(res => {
+    if (!res.ok) {
+        throw new Error(`Error: ${res.status} - ${res.statusText}`);
+    }
+    return res.json();
+})
   .then(data => console.log(data))
   .catch(error => console.error('Error fetching data:', error));
