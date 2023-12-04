41b332117ba089c3a209ce9281bfd57c0e5e4ee2
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2023-12-04 22:23:50 +0400

    m

--- js/product-details.js
+++ js/product-details.js
@@ -4,22 +4,31 @@ $(document).ready(function () {
 
     fetchProductDetails(productId);
 
-function fetchProductDetails(productId) {
-    $.ajax({
-        url: `https://dummyjson.com/products/${productId}`,
-        method: 'GET',
-        success: function (response) {
-            displayProductDetails(response);
-        },
-    }};
-}
-function displayProductDetails(product) {
-    var productDetailsContainer = $('#product-details');
-    var productDiv = $('<div class="product-details"></div>');
-    productDiv.html(`<h2>${product.title}</h2>
-                    <p>Description: ${product.description}</p>
-                    <p>Price: $${product.price}</p>
-                    <p>Discount: ${product.discountPercentage}%</p>
-                    <p>Category: ${product.category}</p>
-                    <p>Stock: ${product.stock}</p>`);
-                }        
+    function fetchProductDetails(productId) {
+        $.ajax({
+            url: `https://dummyjson.com/products/${productId}`,
+            method: 'GET',
+            success: function (response) {
+                displayProductDetails(response);
+            },
+        });
+    }
+    function displayProductDetails(product) {
+        var productDetailsContainer = $('#product-details');
+        var productDiv = $('<div class="product-details"></div>');
+        productDiv.html(`<h2>${product.title}</h2>
+                        <p>Description: ${product.description}</p>
+                        <p>Price: $${product.price}</p>
+                        <p>Discount: ${product.discountPercentage}%</p>
+                        <p>Category: ${product.category}</p>
+                        <p>Stock: ${product.stock}</p>`);
+
+        var imageContainer = $('<div class="image-container"></div>');
+        $.each(product.images, function (index, image) {
+            imageContainer.append(`<img src="${image}" alt="${product.title}">`);
+        });
+
+        productDiv.append(imageContainer);
+        productDetailsContainer.append(productDiv);
+    }
+});
