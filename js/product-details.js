cbdd06e573f9452d374e3b74ebaf05a7fc4f82d9
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2023-12-04 22:15:29 +0400

    m

--- js/product-details.js
+++ js/product-details.js
@@ -14,4 +14,12 @@ function fetchProductDetails(productId) {
     }};
 }
 function displayProductDetails(product) {
-}
+    var productDetailsContainer = $('#product-details');
+    var productDiv = $('<div class="product-details"></div>');
+    productDiv.html(`<h2>${product.title}</h2>
+                    <p>Description: ${product.description}</p>
+                    <p>Price: $${product.price}</p>
+                    <p>Discount: ${product.discountPercentage}%</p>
+                    <p>Category: ${product.category}</p>
+                    <p>Stock: ${product.stock}</p>`);
+                }        
