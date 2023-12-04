1b3b02ebf27eeaf5c6f82c81aba017c5eaf291ee
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2023-12-04 23:15:51 +0400

    m

--- js/script.js
+++ js/script.js
@@ -1,64 +1,70 @@
 $(document).ready(function () {
+    const itemsPerPage = 10;
+    let currentPage = 1;
+
     fetchData();
 
     function fetchData(searchQuery = '', categoryFilter = '') {
+        const apiUrl = 'https://dummyjson.com/products';
+        const startIndex = (currentPage - 1) * itemsPerPage;
+
         $.ajax({
-            url: 'https://dummyjson.com/products',
+            url: apiUrl,
             method: 'GET',
             success: function (response) {
-                displayProducts(response, searchQuery, categoryFilter);
+                const filteredProducts = filterAndPaginate(response.products, startIndex, itemsPerPage, searchQuery, categoryFilter);
+                displayProducts(filteredProducts);
                 populateCategoryFilter(response);
+                displayPagination(response.products.length);
             },
             error: function (error) {
                 console.error('Error fetching data:', error);
             }
         });
     }
-
-    function displayProducts(response, searchQuery, categoryFilter) {
-        var products = response.products || [];
-        var productList = $('#product-list').empty();
+    function displayProducts(products) {
+        const productList = $('#product-list').empty();
 
         $.each(products, function (index, product) {
-            if ((searchQuery === '' || containsSearchQuery(product, searchQuery)) &&
-                (categoryFilter === '' || product.category === categoryFilter)) {
-                var productDiv = $('<div class="product" data-id="' + product.id + '"></div>');
-                productDiv.html(`<h3>${product.title}</h3>
-                                <p>Price: $${product.price}</p>
-                                <p>Discount: ${product.discountPercentage}%</p>
-                                <p>Category: ${product.category}</p>
-                                <p>Stock: ${product.stock}</p>
-                                <img src="${product.thumbnail}" alt="${product.title}">`);
+            const productDiv = $('<div class="product" data-id="' + product.id + '"></div>');
+            
+            productDiv.html(`<h3>${product.title}</h3>
+                            <p>Price: $${product.price}</p>
+                            <p>Discount: ${product.discountPercentage}%</p>
+                            <p>Category: ${product.category}</p>
+                            <p>Stock: ${product.stock}</p>
+                            <img src="${product.thumbnail}" alt="${product.title}">`);
 
-                productDiv.click(function () {
-                    var productId = $(this).data('id');
-                    window.location.href = `product-details.html?id=${productId}`;
-                });
+            productDiv.click(function () {
+                const productId = $(this).data('id');
+                window.location.href = `product-details.html?id=${productId}`;
+            });
 
-                productList.append(productDiv);
-            }
+            productList.append(productDiv);
         });
     }
 
     function containsSearchQuery(product, searchQuery) {
-        return (product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-                product.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
-                product.category.toLowerCase().includes(searchQuery.toLowerCase()));
+        return (
+            product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            product.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            product.category.toLowerCase().includes(searchQuery.toLowerCase())
+        );
     }
 
     function populateCategoryFilter(response) {
-        var categories = [...new Set(response.products.map(product => product.category))];
-        var categoryFilter = $('#category-filter').empty();
+        const categories = [...new Set(response.products.map(product => product.category))];
+        const categoryFilter = $('#category-filter').empty();
         categoryFilter.append('<option value="">All Categories</option>');
-        
+
         $.each(categories, function (index, category) {
             categoryFilter.append(`<option value="${category}">${category}</option>`);
         });
     }
 
     window.applyFilters = function () {
-        var searchQuery = $('#search').val();
-        var categoryFilter = $('#category-filter').val();
+        const searchQuery = $('#search').val();
+        const categoryFilter = $('#category-filter').val();
         fetchData(searchQuery, categoryFilter);
     };
 });
