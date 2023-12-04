6eeed5692d4e314c42dfa86d07c16d7e98461c4a
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2023-12-04 21:57:43 +0400

    m

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
             }
         });
     }
+
     function displayProducts(response, searchQuery, categoryFilter) {
         var products = response.products || [];
         var productList = $('#product-list').empty();
@@ -22,38 +23,39 @@ $(document).ready(function () {
             if ((searchQuery === '' || containsSearchQuery(product, searchQuery)) &&
                 (categoryFilter === '' || product.category === categoryFilter)) {
                 var productDiv = $('<div class="product" data-id="' + product.id + '"></div>');
-
                 productDiv.html(`<h3>${product.title}</h3>
                                 <p>Price: $${product.price}</p>
                                 <p>Discount: ${product.discountPercentage}%</p>
                                 <p>Category: ${product.category}</p>
                                 <p>Stock: ${product.stock}</p>
                                 <img src="${product.thumbnail}" alt="${product.title}">`);
-                               productDiv.click(function () {
-                                var productId = $(this).data('id');
-                                window.location.href = `product-details.html?id=${productId}`;
-                            });
-                            productDiv.click(function () {
-                                var productId = $(this).data('id');
-                                window.location.href = `product-details.html?id=${productId}`;
-                            });            
+
+                productDiv.click(function () {
+                    var productId = $(this).data('id');
+                    window.location.href = `product-details.html?id=${productId}`;
+                });
+
                 productList.append(productDiv);
             }
         });
     }
+
     function containsSearchQuery(product, searchQuery) {
         return (product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
                 product.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
                 product.category.toLowerCase().includes(searchQuery.toLowerCase()));
     }
+
     function populateCategoryFilter(response) {
         var categories = [...new Set(response.products.map(product => product.category))];
         var categoryFilter = $('#category-filter').empty();
         categoryFilter.append('<option value="">All Categories</option>');
+        
         $.each(categories, function (index, category) {
             categoryFilter.append(`<option value="${category}">${category}</option>`);
         });
     }
+
     window.applyFilters = function () {
         var searchQuery = $('#search').val();
         var categoryFilter = $('#category-filter').val();
