74d4d2da45036fdd8a4953ca2bc6d49b7cc62392
GulanaIsmayilova <gismayilova14174@ada.edu.az>
2023-12-04 22:00:27 +0400

    m

--- js/product-details.js
+++ js/product-details.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
     var urlParams = new URLSearchParams(window.location.search);
     var productId = urlParams.get('id');
-    
+
+    fetchProductDetails(productId);
+
 function fetchProductDetails(productId) {
     $.ajax({
         url: `https://dummyjson.com/products/${productId}`,
         method: 'GET',
         success: function (response) {
             displayProductDetails(response);
-        }
-    }}
+        },
+    }};
+}
+function displayProductDetails(product) {
+}
